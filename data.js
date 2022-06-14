e49fca8d347539585a3641f766e28ea2acd70922
feophan <31542979+feophan@users.noreply.github.com>
2022-06-14 22:27:49 +0200

    original

---
+++ data.js
@@ -0,0 +1,183 @@
+const myData = 
+[
+  {
+    "group": "LVS",
+    "data": [
+      {
+        "label": "LVS1",
+        "data": [
+          {
+            "timeRange": ["1969-04-06", "1969-04-07"],
+            "val": "OG",
+            "note": "6-Apr-69"
+          }
+        ]
+      },
+      {
+        "label": "LVS2",
+        "data": [
+          {
+            "timeRange": ["1969-01-01", "1969-04-31"],
+            "val": "OG",
+            "note": "Jan-Apr '69"
+          }
+        ]
+      },
+      {
+        "label": "LVS3",
+        "data": [
+          {
+            "timeRange": ["1969-01-01", "1969-04-31"],
+            "val": "OG",
+            "note": "Jan-Apr '69"
+          }
+        ]
+      },
+      {
+        "label": "LVS4",
+        "data": [
+          {
+            "timeRange": ["1969-03-01", "1969-05-31"],
+            "val": "OG",
+            "note": "spring '69"
+          }
+        ]
+      },
+      {
+        "label": "LVS5",
+        "data": [
+          {
+            "timeRange": ["1969-03-01", "1969-05-31"],
+            "val": "OG",
+            "note": "spring '69"
+          }
+        ]
+      },
+      {
+        "label": "LVS6",
+        "data": [
+          {
+            "timeRange": ["1969-03-01", "1969-05-31"],
+            "val": "OG",
+            "note": "spring '69"
+          }
+        ]
+      },
+      {
+        "label": "LVS7",
+        "data": [
+          {
+            "timeRange": ["1969-04-01", "1969-04-31"],
+            "val": "OG",
+            "note": "spring '69"
+          },
+          {
+            "timeRange": ["1969-05-01", "1969-05-31"],
+            "val": "ADD",
+            "note": "spring '69"
+          }
+        ]
+      },
+      {
+        "label": "LVS8",
+        "data": [
+          {
+            "timeRange": ["1969-03-01", "1969-03-31"],
+            "val": "OG",
+            "note": "spring '69 before LVS7"
+          }
+        ]
+      },
+      {
+        "label": "LVS9",
+        "data": [
+          {
+            "timeRange": ["1969-03-01", "1969-05-31"],
+            "val": "OG",
+            "note": "spring '69"
+          }
+        ]
+      },
+      {
+        "label": "LVS10",
+        "data": [
+          {
+            "timeRange": ["1969-06-01", "1969-08-31"],
+            "val": "OG",
+            "note": "summer '69"
+          }
+        ]
+      },
+      {
+        "label": "LVS11",
+        "data": [
+          {
+            "timeRange": ["1969-06-01", "1969-08-31"],
+            "val": "OG",
+            "note": "summer '69"
+          }
+        ]
+      },
+      {
+        "label": "LVS12",
+        "data": [
+          {
+            "timeRange": ["1969-06-05", "1969-08-31"],
+            "val": "OG",
+            "note": "after 05-Jun-69"
+          }
+        ]
+      },
+      {
+        "label": "LVS13",
+        "data": [
+          {
+            "timeRange": ["1969-09-01", "1969-11-30"],
+            "val": "OG",
+            "note": "fall '69"
+          }
+        ]
+      },
+      {
+        "label": "LVS14",
+        "data": [
+          {
+            "timeRange": ["1969-09-01", "1969-11-30"],
+            "val": "OG",
+            "note": "fall '69"
+          }
+        ]
+      }
+]
+  },
+  {
+    "group": "OP",
+    "data": [
+      {
+        "label": "OP1",
+        "data": [
+          {
+            "timeRange": ["1930-01-01", "1939-12-31"],
+            "val": "OG",
+            "note": "30s"
+          }
+        ]
+      },
+      {
+        "label": "OP2",
+        "data": [
+          {
+            "timeRange": ["1950-01-01", "1959-12-31"],
+            "val": "OG",
+            "note": "50s"
+          },
+          {
+            "timeRange": ["1970-01-01", "1970-12-31"],
+            "val": "GB",
+            "note": "'70'"
+          }
+        ]
+      }
+    ]
+  }
+]
